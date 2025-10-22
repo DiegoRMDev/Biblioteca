49a439777c5c61dd27858cda9cf22160cb761b09
DiegoRMDev <U22222890@utp.edu.pe>
2025-10-22 11:58:42 -0500

    Diseño simple de Dashboard (incompleto)

--- src/main/java/org/biblioteca/presentacion/VentanaPrincipal.java
+++ src/main/java/org/biblioteca/presentacion/VentanaPrincipal.java
@@ -3,4 +3,14 @@ package org.biblioteca.presentacion;
 import javax.swing.*;
 
 public class VentanaPrincipal extends JFrame {
+    private JPanel mainPanel;
+    private JPanel panelIzquierdo;
+    private JPanel panelNorte;
+    private JPanel contentPanel;
+    private JButton btnInicio;
+    private JButton btnTrabajadores;
+    private JButton btnLibros;
+    private JButton btnPrestamos;
+    private JButton btnDevoluciones;
+    private JButton btnReportes;
 }
