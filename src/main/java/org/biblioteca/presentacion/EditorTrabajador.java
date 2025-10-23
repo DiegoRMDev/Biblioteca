4c4aa5777bb38b60a67cf92a7af178c5e7b927cd
DiegoRMDev <U22222890@utp.edu.pe>
2025-10-23 02:24:07 -0500

    diseño basico de trabajador editor y gestion

---
+++ src/main/java/org/biblioteca/presentacion/EditorTrabajador.java
@@ -0,0 +1,14 @@
+package org.biblioteca.presentacion;
+
+import javax.swing.*;
+
+public class EditorTrabajador {
+    private JPanel mainPanel;
+    private JPanel centralPanel;
+    private JButton btnGuardar;
+    private JButton btnCancelar;
+    private JTextField txtNombre;
+    private JTextField txtUsuarioLogin;
+    private JPasswordField txtContrasena;
+    private JComboBox cboRol;
+}
---
+++ src/main/java/org/biblioteca/presentacion/GestionTrabajador.java
@@ -0,0 +1,12 @@
+package org.biblioteca.presentacion;
+
+import javax.swing.*;
+
+public class GestionTrabajador {
+    private JPanel mainPanel;
+    private JTable tablaTrabajadores;
+    private JScrollPane centralPanel;
+    private JButton btnNuevo;
+    private JButton btnEditar;
+    private JButton btnEliminar;
+}
